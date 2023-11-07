be80bef5b6925cf54b245686eddeacd002d087a3
Starmand01 <99566256+Starmand01@users.noreply.github.com>
2023-11-06 21:33:37 -0600

    Initial commit

---
+++ PuntodeVenta/PuntodeVenta.Datos/D_MesaAbierta.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using PuntodeVenta.Entidades;
+using System.Net.Configuration;
+
+namespace PuntodeVenta.Datos
+{
+    public class D_MesaAbierta
+    {
+        public DataTable Listar_SubFamilias_RP(int nCodigo_pv)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Listar_SubFamilias_RP", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@nCodigo_pv", SqlDbType.Int).Value = nCodigo_pv;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+         public DataTable ListarProductos_SubFamilias_RP(int nCodigo_pv, int nCodigo_sf)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_ListarProductos_SubFamilias_RP", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@nCodigo_pv", SqlDbType.Int).Value = nCodigo_pv;
+                Comando.Parameters.Add("@nCodigo_sf", SqlDbType.Int).Value = nCodigo_sf;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public DataTable Busquedarapida_pr(string cTexto)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Busquedarapida_pr", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@cTexto", SqlDbType.VarChar).Value = cTexto;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public DataTable Busqueda_cl(string cTexto)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Busqueda_cl", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@cTexto", SqlDbType.VarChar).Value = cTexto;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public DataTable Guadar_RP(E_RegistroPedido oRP,DataTable Detalle_ticket)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Guardar_Ticket", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@fFecha_emision", SqlDbType.VarChar).Value = oRP.Fecha_emision;
+                Comando.Parameters.Add("@nCodigo_cl", SqlDbType.Int).Value = oRP.Codigo_cl;
+                Comando.Parameters.Add("@cNrodocumento_cl", SqlDbType.VarChar).Value = oRP.Nrodocumento_cl;
+                Comando.Parameters.Add("@cCliente", SqlDbType.VarChar).Value = oRP.Cliente;
+                Comando.Parameters.Add("@nCodigo_me", SqlDbType.Int).Value = oRP.Codigo_me;
+                Comando.Parameters.Add("@dTotal_ti", SqlDbType.Decimal).Value = oRP.Total_ti;
+                Comando.Parameters.Add("@nCodigo_tu", SqlDbType.Int).Value = oRP.Codigo_tu;
+                Comando.Parameters.Add("@nCodigo_us", SqlDbType.Int).Value = oRP.Codigo_us;
+                Comando.Parameters.Add("@tDetalle", SqlDbType.Structured).Value = Detalle_ticket;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public DataTable Imprimir_comanda(string cImpresora,int nCodigo_ti)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Imprimir_comanda", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@cImpresora", SqlDbType.VarChar).Value = cImpresora;
+                Comando.Parameters.Add("@nCodigo_ti", SqlDbType.Int).Value = nCodigo_ti;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public DataTable Mostrar_Tickets_Mesa(int nCodigo_me)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Mostrar_Tickets_Mesa", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@nCodigo_me", SqlDbType.Int).Value = nCodigo_me;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public DataTable Mostrar_ticket(int nCodigo_ti)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Mostrar_ticket", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@nCodigo_ti", SqlDbType.Int).Value = nCodigo_ti;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public DataTable ReImprimir_comanda(int nCodigo_ti)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Reimprimir_comanda", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;              
+                Comando.Parameters.Add("@nCodigo_ti", SqlDbType.Int).Value = nCodigo_ti;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public DataTable Usuario_Admin(int nCodigo_us)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Usuario_Admin", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@nCodigo_us", SqlDbType.Int).Value = nCodigo_us;
+                SqlCon.Open();
+                Resultado = Comando.ExecuteReader();
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+
+        public string Eliminar_ti(int nCodigo_ti, int nCodigo_me, string cObservacion)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("USP_Eliminar_ti",SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                Comando.Parameters.Add("@nCodigo_ti",SqlDbType.Int).Value = nCodigo_ti;
+                Comando.Parameters.Add("@nCodigo_me", SqlDbType.Int).Value = nCodigo_me;
+                Comando.Parameters.Add("@cObsanulado_ti", SqlDbType.Text).Value = cObservacion;
+                SqlCon.Open();
+                Rpta = Comando.ExecuteNonQuery() > 0 ? "OK" : "No se pudo anular el ticket";               
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
+        }
+        
+    }
+}
---
+++ PuntodeVenta/PuntodeVenta.Negocio/N_MesaAbierta.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using PuntodeVenta.Entidades;
+using PuntodeVenta.Datos;
+
+namespace PuntodeVenta.Negocio
+{
+    public class N_MesaAbierta
+    {
+        public static DataTable Listar_SubFamilias_RP(int nCodigo_pv)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.Listar_SubFamilias_RP(nCodigo_pv);
+        }
+
+        public static DataTable ListarProductos_SubFamilias_RP(int nCodigo_pv, int nCodigo_sf)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.ListarProductos_SubFamilias_RP(nCodigo_pv,nCodigo_sf);
+        }
+
+        public static DataTable Busquedarapida_pr(string cTexto)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.Busquedarapida_pr(cTexto);
+        }
+
+        public static DataTable Busqueda_cl(string cTexto)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.Busqueda_cl(cTexto);
+        }
+
+        public static DataTable Guardar_RP(E_RegistroPedido oRP, DataTable Detalle_ticket)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.Guadar_RP(oRP, Detalle_ticket);
+        }
+
+        public static DataTable Imprimir_comanda(string cImpresora, int nCodigo_ti)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.Imprimir_comanda(cImpresora, nCodigo_ti);
+        }
+
+        public static DataTable Mostrar_Tickets_Mesa(int nCodigo_me)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.Mostrar_Tickets_Mesa(nCodigo_me);
+        }
+
+        public static DataTable Mostrar_ticket(int nCodigo_ti)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.Mostrar_ticket(nCodigo_ti);
+        }
+
+        public static DataTable ReImprimir_comanda(int nCodigo_ti)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.ReImprimir_comanda(nCodigo_ti);
+        }
+
+        public static DataTable Usuario_Admin(int nCodigo_us)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.Usuario_Admin(nCodigo_us);
+        }
+
+        public static string Eliminar_ti(int nCodigo_ti, int nCodigo_me, string cObservacion)
+        {
+            D_MesaAbierta Datos = new D_MesaAbierta();
+            return Datos.Eliminar_ti(nCodigo_ti, nCodigo_me, cObservacion);
+        }
+    }
+}
